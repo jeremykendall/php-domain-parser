e0144ce0efc28107f293538f737b3c4d0bf26066
Jeremy Kendall <jeremy@jeremykendall.net>
2013-01-13 08:47:27 -0600

    Refactors PublicSuffixList to extend ArrayObject, removes test

--- library/Pdp/PublicSuffixList.php
+++ library/Pdp/PublicSuffixList.php
@@ -13,51 +13,6 @@ namespace Pdp;
 /**
  * Public Suffix List
  */
-class PublicSuffixList
+class PublicSuffixList extends \ArrayObject
 {
-    /**
-     * @var array Public Suffix List
-     */
-    protected $list;
-
-    /**
-     * Public constructor
-     *
-     * @param array $list Public Suffix List
-     */
-    public function __construct(array $list)
-    {
-        $this->list = $list;
-    }
-
-    /**
-     * Counts element in list
-     *
-     * @return int The number of elements in $list
-     */
-    public function count()
-    {
-        return count($this->list);
-    }
-
-    /**
-     * Searches list for value
-     *
-     * @param mixed Value to search for
-     * @return int|false Returns key for value if found, false otherwise
-     */
-    public function search($value)
-    {
-        return array_search($value, $this->list);
-    }
-
-    /**
-     * Gets list
-     *
-     * @return array Public Suffix List
-     */
-    public function getList()
-    {
-        return $this->list;
-    }
 }
