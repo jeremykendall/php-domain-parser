ec7866a625cacae846e60e905adfb9f0f3b6b9ef
Jeremy Kendall <jkendall@opensky.com>
2014-02-02 11:08:54 -0600

    Updates copyright/license year

--- library/Pdp/HttpAdapter/HttpAdapterInterface.php
+++ library/Pdp/HttpAdapter/HttpAdapterInterface.php
@@ -4,7 +4,7 @@
  * PHP Domain Parser: Public Suffix List based URL parsing
  *
  * @link      http://github.com/jeremykendall/php-domain-parser for the canonical source repository
- * @copyright Copyright (c) 2013 Jeremy Kendall (http://about.me/jeremykendall)
+ * @copyright Copyright (c) 2014 Jeremy Kendall (http://about.me/jeremykendall)
  * @license   http://github.com/jeremykendall/php-domain-parser/blob/master/LICENSE MIT License
  */
 
