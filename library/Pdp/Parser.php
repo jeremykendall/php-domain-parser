428718fedf3faefd65f5d2aadd4d44694dc4f2e0
jeremykendall <jeremy@jeremykendall.net>
2014-07-20 17:11:12 -0500

    Refactors mbParseUrl to allow for use of $component param

--- library/Pdp/Parser.php
+++ library/Pdp/Parser.php
@@ -230,13 +230,19 @@ class Parser
     }
 
     /**
-     * UTF-8 aware parse_url() replacement. Taken from php.net manual comments.
+     * UTF-8 aware parse_url() replacement. 
      *
-     * @link http://php.net/manual/en/function.parse-url.php#114817
+     * Taken from php.net manual comments {@link http://php.net/manual/en/function.parse-url.php#114817}
      *
-     * @return array
+     * @param  string  $url       The URL to parse
+     * @param  integer $component Specify one of PHP_URL_SCHEME, PHP_URL_HOST,
+     *                            PHP_URL_PORT, PHP_URL_USER, PHP_URL_PASS, PHP_URL_PATH, PHP_URL_QUERY or
+     *                            PHP_URL_FRAGMENT to retrieve just a specific URL component as a string
+     *                            (except when PHP_URL_PORT is given, in which case the return value will
+     *                            be an integer).
+     * @return mixed   See parse_url documentation {@link http://us1.php.net/parse_url}
      */
-    protected function mbParseUrl($url)
+    public function mbParseUrl($url, $component = -1)
     {
         $enc_url = preg_replace_callback(
             '%[^:/@?&=#]+%usD',
@@ -246,14 +252,18 @@ class Parser
             $url
         );
 
-        $parts = parse_url($enc_url);
+        $parts = parse_url($enc_url, $component);
 
         if ($parts === false) {
             throw new \InvalidArgumentException(sprintf('Invalid url %s', $url));
         }
 
-        foreach ($parts as $name => $value) {
-            $parts[$name] = urldecode($value);
+        if (is_array($parts)) {
+            foreach ($parts as $name => $value) {
+                $parts[$name] = urldecode($value);
+            }
+        } else {
+            $parts = urldecode($parts);
         }
 
         return $parts;
