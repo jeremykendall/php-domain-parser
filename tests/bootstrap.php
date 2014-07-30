c15e72811794a7554087eaed28d3e49d5dc8b458
jeremykendall <jeremy@jeremykendall.net>
2014-07-30 14:57:34 -0500

    Removes unused debug functions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,14 +7,3 @@ ini_set('display_startup_errors', 1);
 date_default_timezone_set('UTC');
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-function d($expression)
-{
-    var_dump($expression);
-}
-
-function dd($expression)
-{
-    d($expression);
-    die();
-}
