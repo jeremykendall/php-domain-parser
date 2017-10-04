90a3a86e88b8b184fc2df05c64d6f11c409e6900
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2017-10-04 15:10:25 +0200

    typo fix between repo

--- src/Pdp/LabelsTrait.php
+++ src/Pdp/LabelsTrait.php
@@ -2,10 +2,10 @@
 /**
  * Public Suffix List PHP: Public Suffix List based URL parsing.
  *
- * @see http://github.com/jeremykendall/publicsuffixlist-php for the canonical source repository
+ * @see http://github.com/jeremykendall/php-domain-parser for the canonical source repository
  *
  * @copyright Copyright (c) 2017 Jeremy Kendall (http://jeremykendall.net)
- * @license   http://github.com/jeremykendall/publicsuffixlist-php/blob/master/LICENSE MIT License
+ * @license   http://github.com/jeremykendall/php-domain-parser/blob/master/LICENSE MIT License
  */
 declare(strict_types=1);
 
