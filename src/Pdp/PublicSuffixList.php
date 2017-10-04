d972f37c5c96fbd394dce92bdda80870654bf809
jeremykendall <jeremy@jeremykendall.net>
2017-10-03 22:46:25 -0500

    New version of PublicSuffixList

--- src/Pdp/PublicSuffixList.php
+++ src/Pdp/PublicSuffixList.php
@@ -1,31 +1,191 @@
 <?php
 
+declare(strict_types=1);
+
 /**
- * PHP Domain Parser: Public Suffix List based URL parsing.
+ * Public Suffix List PHP: Public Suffix List based URL parsing.
  *
- * @link      http://github.com/jeremykendall/php-domain-parser for the canonical source repository
+ * @see http://github.com/jeremykendall/publicsuffixlist-php for the canonical source repository
  *
- * @copyright Copyright (c) 2014 Jeremy Kendall (http://about.me/jeremykendall)
- * @license   http://github.com/jeremykendall/php-domain-parser/blob/master/LICENSE MIT License
+ * @copyright Copyright (c) 2017 Jeremy Kendall (http://jeremykendall.net)
+ * @license   http://github.com/jeremykendall/publicsuffixlist-php/blob/master/LICENSE MIT License
  */
 namespace Pdp;
 
-/**
- * Public Suffix List.
- */
-class PublicSuffixList extends \ArrayObject
+final class PublicSuffixList implements \Countable
 {
+    use LabelsTrait;
+
     /**
-     * Public constructor.
-     *
-     * @param mixed $list Array representing Public Suffix List or PHP Public Suffix List file
+     * @var array
      */
-    public function __construct($list)
+    private $rules;
+
+    /**
+     * PublicSuffixList constructor.
+     * @param mixed $rules
+     */
+    public function __construct($rules = null)
     {
-        if (!is_array($list)) {
-            $list = include $list;
+        if (is_string($rules) && file_exists($rules) && is_readable($rules)) {
+            $this->rules = include $rules;
         }
 
-        parent::__construct($list);
+        if ($rules === null) {
+            $this->rules = include dirname(__DIR__, 2) . '/data/public-suffix-list.php';
+        }
+
+        if (is_array($rules)) {
+            $this->rules = $rules;
+        }
+        $this->rules = $rules ?? include dirname(__DIR__, 2) . '/data/public-suffix-list.php';
+    }
+
+    public function query(string $domain = null): Domain
+    {
+        if (!$this->isMatchable($domain)) {
+            return new NullDomain();
+        }
+
+        $input = $domain;
+        $domain = $this->normalize($domain);
+        $matchingLabels = $this->findMatchingLabels($this->getLabelsReverse($domain), $this->rules);
+        $publicSuffix = empty($matchingLabels) ? $this->handleNoMatches($domain) : $this->processMatches($matchingLabels);
+
+        if ($this->isPunycoded($input) === false) {
+            $publicSuffix = idn_to_utf8($publicSuffix, 0, INTL_IDNA_VARIANT_UTS46);
+        }
+
+        if (count($matchingLabels) > 0) {
+            return new MatchedDomain($input, $publicSuffix, true);
+        }
+
+        return new UnmatchedDomain($input, $publicSuffix, false);
+    }
+
+    public function getRules(): array
+    {
+        return $this->rules;
+    }
+
+    /**
+     * @TODO: Remove. Bandaid to fix failing test.
+     *
+     * @return int
+     */
+    public function count(): int
+    {
+        return count($this->rules);
+    }
+
+    private function isMatchable($domain): bool
+    {
+        if ($domain === null) {
+            return false;
+        }
+
+        if ($this->hasLeadingDot($domain)) {
+            return false;
+        }
+
+        if ($this->isSingleLabelDomain($domain)) {
+            return false;
+        }
+
+        if ($this->isIpAddress($domain)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Normalize domain.
+     *
+     * "The domain must be canonicalized in the normal way for hostnames - lower-case, Punycode."
+     *
+     * @see http://www.ietf.org/rfc/rfc3492.txt
+     *
+     * @param string $domain
+     *
+     * @return string
+     */
+    private function normalize(string $domain): string
+    {
+        return strtolower(idn_to_ascii($domain, 0, INTL_IDNA_VARIANT_UTS46));
+    }
+
+    private function findMatchingLabels(array $labels, array $rules): array
+    {
+        $matches = [];
+
+        foreach ($labels as $label) {
+            if ($this->isExceptionRule($label, $rules)) {
+                break;
+            }
+
+            if ($this->isWildcardRule($rules)) {
+                array_unshift($matches, $label);
+                break;
+            }
+
+            if ($this->matchExists($label, $rules)) {
+                array_unshift($matches, $label);
+                $rules = $rules[$label];
+                continue;
+            }
+
+            // Avoids improper parsing when $domain's subdomain + public suffix ===
+            // a valid public suffix (e.g. domain 'us.example.com' and public suffix 'us.com')
+            //
+            // Added by @goodhabit in https://github.com/jeremykendall/php-domain-parser/pull/15
+            // Resolves https://github.com/jeremykendall/php-domain-parser/issues/16
+            break;
+        }
+
+        return $matches;
+    }
+
+    private function processMatches(array $matches): string
+    {
+        return implode('.', array_filter($matches, 'strlen'));
+    }
+
+    private function isIpAddress(string $domain): bool
+    {
+        return filter_var($domain, FILTER_VALIDATE_IP) !== false;
+    }
+
+    private function isExceptionRule(string $label, array $rules): bool
+    {
+        return $this->matchExists($label, $rules)
+            && array_key_exists('!', $rules[$label]);
+    }
+
+    private function isWildcardRule(array $rules): bool
+    {
+        return array_key_exists('*', $rules);
+    }
+
+    private function matchExists(string $label, array $rules): bool
+    {
+        return array_key_exists($label, $rules);
+    }
+
+    private function handleNoMatches(string $domain): string
+    {
+        $labels = $this->getLabels($domain);
+
+        return array_pop($labels);
+    }
+
+    private function isPunycoded(string $input): bool
+    {
+        return strpos($input, 'xn--') !== false;
+    }
+
+    private function hasLeadingDot($domain): bool
+    {
+        return strpos($domain, '.') === 0;
     }
 }
---
+++ tests/src/Pdp/PublicSuffixListTest.php
@@ -0,0 +1,219 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Public Suffix List PHP: Public Suffix List based URL parsing.
+ *
+ * @see http://github.com/jeremykendall/publicsuffixlist-php for the canonical source repository
+ *
+ * @copyright Copyright (c) 2017 Jeremy Kendall (http://jeremykendall.net)
+ * @license   http://github.com/jeremykendall/publicsuffixlist-php/blob/master/LICENSE MIT License
+ */
+namespace Pdp;
+
+use PHPUnit\Framework\TestCase;
+
+class PublicSuffixListTest extends TestCase
+{
+    /**
+     * @var PublicSuffixList
+     */
+    private $list;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->list = new PublicSuffixList();
+    }
+
+    public function testNullWillReturnNullDomain()
+    {
+        $domain = $this->list->query('COM');
+        $this->assertFalse($domain->isValid());
+        $this->assertInstanceOf(NullDomain::class, $domain);
+    }
+
+    public function testIsSuffixValidFalse()
+    {
+        $domain = $this->list->query('www.example.faketld');
+        $this->assertFalse($domain->isValid());
+        $this->assertInstanceOf(UnmatchedDomain::class, $domain);
+    }
+
+    public function testIsSuffixValidTrue()
+    {
+        $domain = $this->list->query('www.example.com');
+        $this->assertTrue($domain->isValid());
+        $this->assertInstanceOf(MatchedDomain::class, $domain);
+    }
+
+    /**
+     * @dataProvider parseDataProvider
+     */
+    public function testGetRegistrableDomain($publicSuffix, $registrableDomain, $domain)
+    {
+        $this->assertSame($registrableDomain, $this->list->query($domain)->getRegistrableDomain());
+    }
+
+    /**
+     * @dataProvider parseDataProvider
+     */
+    public function testGetPublicSuffix($publicSuffix, $registrableDomain, $domain)
+    {
+        $this->assertSame($publicSuffix, $this->list->query($domain)->getPublicSuffix());
+    }
+
+    public function testGetPublicSuffixHandlesWrongCaseProperly()
+    {
+        $publicSuffix = 'рф';
+        $domain = 'Яндекс.РФ';
+
+        $this->assertSame($publicSuffix, $this->list->query($domain)->getPublicSuffix());
+    }
+
+    public function testPublicSuffixSpec()
+    {
+        // Test data from Rob Stradling at Comodo
+        // http://mxr.mozilla.org/mozilla-central/source/netwerk/test/unit/data/test_psl.txt?raw=1
+        // Any copyright is dedicated to the Public Domain.
+        // http://creativecommons.org/publicdomain/zero/1.0/
+
+        // null input.
+        $this->checkPublicSuffix(null, null);
+        // Mixed case.
+        $this->checkPublicSuffix('COM', null);
+        $this->checkPublicSuffix('example.COM', 'example.com');
+        $this->checkPublicSuffix('WwW.example.COM', 'example.com');
+        // Leading dot.
+        $this->checkPublicSuffix('.com', null);
+        $this->checkPublicSuffix('.example', null);
+        $this->checkPublicSuffix('.example.com', null);
+        $this->checkPublicSuffix('.example.example', null);
+        // Unlisted TLD.
+        // Addresses algorithm rule #2: If no rules match, the prevailing rule is "*".
+        $this->checkPublicSuffix('example', null);
+        $this->checkPublicSuffix('example.example', 'example.example');
+        $this->checkPublicSuffix('b.example.example', 'example.example');
+        $this->checkPublicSuffix('a.b.example.example', 'example.example');
+        // TLD with only 1 rule.
+        $this->checkPublicSuffix('biz', null);
+        $this->checkPublicSuffix('domain.biz', 'domain.biz');
+        $this->checkPublicSuffix('b.domain.biz', 'domain.biz');
+        $this->checkPublicSuffix('a.b.domain.biz', 'domain.biz');
+        // TLD with some 2-level rules.
+        $this->checkPublicSuffix('com', null);
+        $this->checkPublicSuffix('example.com', 'example.com');
+        $this->checkPublicSuffix('b.example.com', 'example.com');
+        $this->checkPublicSuffix('a.b.example.com', 'example.com');
+        $this->checkPublicSuffix('uk.com', null);
+        $this->checkPublicSuffix('example.uk.com', 'example.uk.com');
+        $this->checkPublicSuffix('b.example.uk.com', 'example.uk.com');
+        $this->checkPublicSuffix('a.b.example.uk.com', 'example.uk.com');
+        $this->checkPublicSuffix('test.ac', 'test.ac');
+        // TLD with only 1 (wildcard) rule.
+        $this->checkPublicSuffix('mm', null);
+        $this->checkPublicSuffix('c.mm', null);
+        $this->checkPublicSuffix('b.c.mm', 'b.c.mm');
+        $this->checkPublicSuffix('a.b.c.mm', 'b.c.mm');
+        // More complex TLD.
+        $this->checkPublicSuffix('jp', null);
+        $this->checkPublicSuffix('test.jp', 'test.jp');
+        $this->checkPublicSuffix('www.test.jp', 'test.jp');
+        $this->checkPublicSuffix('ac.jp', null);
+        $this->checkPublicSuffix('test.ac.jp', 'test.ac.jp');
+        $this->checkPublicSuffix('www.test.ac.jp', 'test.ac.jp');
+        $this->checkPublicSuffix('kyoto.jp', null);
+        $this->checkPublicSuffix('test.kyoto.jp', 'test.kyoto.jp');
+        $this->checkPublicSuffix('ide.kyoto.jp', null);
+        $this->checkPublicSuffix('b.ide.kyoto.jp', 'b.ide.kyoto.jp');
+        $this->checkPublicSuffix('a.b.ide.kyoto.jp', 'b.ide.kyoto.jp');
+        $this->checkPublicSuffix('c.kobe.jp', null);
+        $this->checkPublicSuffix('b.c.kobe.jp', 'b.c.kobe.jp');
+        $this->checkPublicSuffix('a.b.c.kobe.jp', 'b.c.kobe.jp');
+        $this->checkPublicSuffix('city.kobe.jp', 'city.kobe.jp');
+        $this->checkPublicSuffix('www.city.kobe.jp', 'city.kobe.jp');
+        // TLD with a wildcard rule and exceptions.
+        $this->checkPublicSuffix('ck', null);
+        $this->checkPublicSuffix('test.ck', null);
+        $this->checkPublicSuffix('b.test.ck', 'b.test.ck');
+        $this->checkPublicSuffix('a.b.test.ck', 'b.test.ck');
+        $this->checkPublicSuffix('www.ck', 'www.ck');
+        $this->checkPublicSuffix('www.www.ck', 'www.ck');
+        // US K12.
+        $this->checkPublicSuffix('us', null);
+        $this->checkPublicSuffix('test.us', 'test.us');
+        $this->checkPublicSuffix('www.test.us', 'test.us');
+        $this->checkPublicSuffix('ak.us', null);
+        $this->checkPublicSuffix('test.ak.us', 'test.ak.us');
+        $this->checkPublicSuffix('www.test.ak.us', 'test.ak.us');
+        $this->checkPublicSuffix('k12.ak.us', null);
+        $this->checkPublicSuffix('test.k12.ak.us', 'test.k12.ak.us');
+        $this->checkPublicSuffix('www.test.k12.ak.us', 'test.k12.ak.us');
+        // IDN labels.
+        $this->checkPublicSuffix('食狮.com.cn', '食狮.com.cn');
+        $this->checkPublicSuffix('食狮.公司.cn', '食狮.公司.cn');
+        $this->checkPublicSuffix('www.食狮.公司.cn', '食狮.公司.cn');
+        $this->checkPublicSuffix('shishi.公司.cn', 'shishi.公司.cn');
+        $this->checkPublicSuffix('公司.cn', null);
+        $this->checkPublicSuffix('食狮.中国', '食狮.中国');
+        $this->checkPublicSuffix('www.食狮.中国', '食狮.中国');
+        $this->checkPublicSuffix('shishi.中国', 'shishi.中国');
+        $this->checkPublicSuffix('中国', null);
+        // Same as above, but punycoded.
+        $this->checkPublicSuffix('xn--85x722f.com.cn', 'xn--85x722f.com.cn');
+        $this->checkPublicSuffix('xn--85x722f.xn--55qx5d.cn', 'xn--85x722f.xn--55qx5d.cn');
+        $this->checkPublicSuffix('www.xn--85x722f.xn--55qx5d.cn', 'xn--85x722f.xn--55qx5d.cn');
+        $this->checkPublicSuffix('shishi.xn--55qx5d.cn', 'shishi.xn--55qx5d.cn');
+        $this->checkPublicSuffix('xn--55qx5d.cn', null);
+        $this->checkPublicSuffix('xn--85x722f.xn--fiqs8s', 'xn--85x722f.xn--fiqs8s');
+        $this->checkPublicSuffix('www.xn--85x722f.xn--fiqs8s', 'xn--85x722f.xn--fiqs8s');
+        $this->checkPublicSuffix('shishi.xn--fiqs8s', 'shishi.xn--fiqs8s');
+        $this->checkPublicSuffix('xn--fiqs8s', null);
+    }
+
+    public function parseDataProvider()
+    {
+        return [
+            // public suffix, registrable domain, domain
+            // BEGIN https://github.com/jeremykendall/php-domain-parser/issues/16
+            ['com', 'example.com', 'us.example.com'],
+            ['na', 'example.na', 'us.example.na'],
+            ['us.na', 'example.us.na', 'www.example.us.na'],
+            ['org', 'example.org', 'us.example.org'],
+            ['biz', 'broken.biz', 'webhop.broken.biz'],
+            ['webhop.biz', 'broken.webhop.biz', 'www.broken.webhop.biz'],
+            // END https://github.com/jeremykendall/php-domain-parser/issues/16
+            // Test ipv6 URL
+            [null, null, '[::1]'],
+            [null, null, '[2001:db8:85a3:8d3:1319:8a2e:370:7348]'],
+            [null, null, '[2001:db8:85a3:8d3:1319:8a2e:370:7348]'],
+            // Test IP address: Fixes #43
+            [null, null, '192.168.1.2'],
+            // Link-local addresses and zone indices
+            [null, null, '[fe80::3%25eth0]'],
+            [null, null, '[fe80::1%2511]'],
+            ['faketld', 'example.faketld', 'example.faketld'],
+        ];
+    }
+
+    /**
+     * Checks PublicSuffixList can return proper registrable domain.
+     *
+     * "You will need to define a checkPublicSuffix() function which takes as a
+     * parameter a domain name and the public suffix, runs your implementation
+     * on the domain name and checks the result is the public suffix expected."
+     *
+     * @see http://publicsuffix.org/list/
+     *
+     * @param string $domain
+     * @param string $expected
+     */
+    private function checkPublicSuffix($domain, $expected)
+    {
+        $this->assertSame(
+            $expected,
+            $this->list->query($domain)->getRegistrableDomain()
+        );
+    }
+}
