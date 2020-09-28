b0b6299842fdfcf8e54c691545e7efe7f4ee3e92
ignace nyamagana butera <nyamsprod@gmail.com>
2020-09-28 21:08:34 +0200

    Reduce Storage interface

--- src/Storage/PsrStorageFactory.php
+++ src/Storage/PsrStorageFactory.php
@@ -22,11 +22,7 @@ use Psr\SimpleCache\CacheInterface;
 
 final class PsrStorageFactory implements
     RemoteStorageURL,
-    PublicSuffixListCacheFactory,
-    PublicSuffixListClientFactory,
     PublicSuffixListStorageFactory,
-    RootZoneDatabaseCacheFactory,
-    RootZoneDatabaseClientFactory,
     RootZoneDatabaseStorageFactory
 {
     private ClientInterface $client;
@@ -49,44 +45,16 @@ final class PsrStorageFactory implements
         $this->logger = $logger;
     }
 
-    public function createPublicSuffixListClient(): PublicSuffixListRepository
-    {
-        return new RulesPsr18Client($this->client, $this->requestFactory);
-    }
-
-    /**
-     * @param mixed $cacheTtl The cache TTL
-     */
-    public function createPublicSuffixListCache(string $cachePrefix = '', $cacheTtl = null): PublicSuffixListCache
-    {
-        return new RulesPsr16Cache(
-            new JsonSerializablePsr16Cache($this->cache, $cachePrefix, $cacheTtl, $this->logger)
-        );
-    }
-
     /**
      * @param mixed $cacheTtl The cache TTL
      */
     public function createPublicSuffixListStorage(string $cachePrefix = '', $cacheTtl = null): PublicSuffixListStorage
     {
         return new RulesRepository(
-            $this->createPublicSuffixListClient(),
-            $this->createPublicSuffixListCache($cachePrefix, $cacheTtl)
-        );
-    }
-
-    public function createRootZoneDatabaseClient(): RootZoneDatabaseRepository
-    {
-        return new TopLevelDomainsPsr18Client($this->client, $this->requestFactory);
-    }
-
-    /**
-     * @param mixed $cacheTtl The cache TTL
-     */
-    public function createRootZoneDatabaseCache(string $cachePrefix = '', $cacheTtl = null): RootZoneDatabaseCache
-    {
-        return new TopLevelDomainsPsr16Cache(
-            new JsonSerializablePsr16Cache($this->cache, $cachePrefix, $cacheTtl, $this->logger)
+            new RulesPsr18Client($this->client, $this->requestFactory),
+            new RulesPsr16Cache(
+                new JsonSerializablePsr16Cache($this->cache, $cachePrefix, $cacheTtl, $this->logger)
+            )
         );
     }
 
@@ -96,8 +64,10 @@ final class PsrStorageFactory implements
     public function createRootZoneDatabaseStorage(string $cachePrefix = '', $cacheTtl = null): RootZoneDatabaseStorage
     {
         return new TopLevelDomainsRepository(
-            $this->createRootZoneDatabaseClient(),
-            $this->createRootZoneDatabaseCache($cachePrefix, $cacheTtl)
+            new TopLevelDomainsPsr18Client($this->client, $this->requestFactory),
+            new TopLevelDomainsPsr16Cache(
+                new JsonSerializablePsr16Cache($this->cache, $cachePrefix, $cacheTtl, $this->logger)
+            )
         );
     }
 }
