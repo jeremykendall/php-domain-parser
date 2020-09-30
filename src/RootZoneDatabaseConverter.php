4fcc68c9ca0dc244a100dd0e0de972a92f930227
ignace nyamagana butera <nyamsprod@gmail.com>
2020-09-30 19:47:05 +0200

    Remove useless copyrights from source code

--- src/RootZoneDatabaseConverter.php
+++ src/RootZoneDatabaseConverter.php
@@ -5,8 +5,6 @@
  *
  * @see http://github.com/jeremykendall/php-domain-parser for the canonical source repository
  *
- * @copyright Copyright (c) 2017 Jeremy Kendall (http://jeremykendall.net)
- *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
