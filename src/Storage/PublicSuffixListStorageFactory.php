87bb6add30ece49e59a29cf3311ff7613b38a925
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2022-02-19 19:35:29 +0100

    Improve storage codebase against PHP8 syntax

--- src/Storage/PublicSuffixListStorageFactory.php
+++ src/Storage/PublicSuffixListStorageFactory.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace Pdp\Storage;
 
 use DateInterval;
-use DateTimeInterface;
-use Stringable;
 
 interface PublicSuffixListStorageFactory
 {
     public function createPublicSuffixListStorage(
         string $cachePrefix = '',
-        DateInterval|DateTimeInterface|Stringable|int|string|null $cacheTtl = null
+        DateInterval|int|null $cacheTtl = null
     ): PublicSuffixListStorage;
 }
--- src/Storage/TopLevelDomainListStorageFactory.php
+++ src/Storage/TopLevelDomainListStorageFactory.php
@@ -1,18 +1,15 @@
 <?php
 
-
 declare(strict_types=1);
 
 namespace Pdp\Storage;
 
 use DateInterval;
-use DateTimeInterface;
-use Stringable;
 
 interface TopLevelDomainListStorageFactory
 {
     public function createTopLevelDomainListStorage(
         string $cachePrefix = '',
-        DateInterval|DateTimeInterface|Stringable|int|string|null $cacheTtl = null
+        DateInterval|int|null $cacheTtl = null
     ): TopLevelDomainListStorage;
 }
